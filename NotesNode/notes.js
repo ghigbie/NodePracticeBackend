aea9556c63d2ec1d703ade294bff2064e98427a8
George Higbie <gehigbie@gmail.com>
2017-03-18 08:09:16 -0700

    remove demo functions and greetings.txt file

--- NotesNode/notes.js
+++ NotesNode/notes.js
@@ -1,12 +1,3 @@
 console.log("Starting notes.js");
 
-module.exports.addNote = () => {
-	console.log("addNote");
-	return "New Note";
-};
-
-module.exports.addNumbers = (a, b) => {
-	console.log("addNumbers called");
-	return a+b;
-};
 
