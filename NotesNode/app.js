22072356f5f67b54bcc9d1019d1984dc7ccd32bf
George Higbie <gehigbie@gmail.com>
2017-03-18 07:21:22 -0700

    initialize npm and install lodash

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -2,7 +2,9 @@ console.log("Starting app.js");
 
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash");
 const notes = require("./notes.js");
+
 
 var res = notes.addNote();
 console.log(res);
