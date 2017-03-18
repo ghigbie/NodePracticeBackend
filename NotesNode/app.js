c53718c06422b682eb1708bc9ce0453b6f3fadd0
George Higbie <gehigbie@gmail.com>
2017-03-17 17:58:15 -0700

    add fs.append for future version of node

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -3,3 +3,11 @@ console.log("Starting App");
 const fs = require("fs");
 
 fs.appendFile("greetings.txt", "Hello Word");
+
+//for version 7.x.x of node
+fs. appendFile("greetings.txt", "Hello World", function(err){
+	if(err){
+		console.log("Unable to write to file");
+		console.log(err);
+	}
+});
