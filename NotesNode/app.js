ccefb90c2287cc20682ed979a35a3c23893bb918
George Higbie <gehigbie@gmail.com>
2017-03-18 08:06:45 -0700

    separate 3rd party/node modules from developer created files

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -1,31 +1,8 @@
 console.log("Starting app.js");
 
+//third-party and node modules
 const fs = require("fs");
-const os = require("os");
 const _ = require("lodash");
+
+//developer created file
 const notes = require("./notes.js");
-
-var filteredArray = _.uniq(["Billy", "Bob", "Billy"]);
-console.log(filteredArray);
-
-
-var res = notes.addNote();
-console.log(res);
-
-var ath = notes.addNumbers(7, 9);
-console.log(ath);
-
-// let user = os.userInfo();
-
-// fs.appendFile("greetings.txt", `Hello ${user.username}! You are ${notes.age}.`);
-
-
-//fs.appendFile("greetings.txt", "Hello Word");
-
-//for version 7.x.x of node
-// fs. appendFile("greetings.txt", `Hello ${user.username}!`, function(err){
-// 	if(err){
-// 		console.log("Unable to write to file");
-// 		console.log(err);
-// 	}
-// });
