7cab75d6ec0289e978ba0b104b5a5fa1bf0f9837
George Higbie <gehigbie@gmail.com>
2017-03-17 18:13:32 -0700

    update fs.appendFile to work properly

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -1,13 +1,19 @@
 console.log("Starting App");
 
 const fs = require("fs");
+const os = require("os");
 
-fs.appendFile("greetings.txt", "Hello Word");
+let user = os.userInfo();
+
+fs.appendFile("greetings.txt", `Hello ${user.username}!`);
+
+
+//fs.appendFile("greetings.txt", "Hello Word");
 
 //for version 7.x.x of node
-fs. appendFile("greetings.txt", "Hello World", function(err){
-	if(err){
-		console.log("Unable to write to file");
-		console.log(err);
-	}
-});
+// fs. appendFile("greetings.txt", `Hello ${user.username}!`, function(err){
+// 	if(err){
+// 		console.log("Unable to write to file");
+// 		console.log(err);
+// 	}
+// });
