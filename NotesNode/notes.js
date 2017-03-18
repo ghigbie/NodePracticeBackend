41625f222a4e82b208eef1f56421e0878831a2ce
George Higbie <gehigbie@gmail.com>
2017-03-18 09:28:31 -0700

    add two new methods to notes.js and call them in app.js

--- NotesNode/notes.js
+++ NotesNode/notes.js
@@ -8,8 +8,19 @@ var getAll = () =>{
 	console.log("Getting all notes...");
 };
 
+var readNote = () => {
+	console.log("YOu can now read ", title, body);
+};
+
+var removeNote = (title) =>{
+	console.log("Removing note: ", title);
+};
+
+
 //this uses ES6 syntax to list out identical key value pairs in an object
 module.exports = {
 	addNote,
-	getAll
+	getAll,
+	readNote,
+	removeNote
 };
