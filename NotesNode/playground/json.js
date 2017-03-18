b848ed10a0e6938a325b02ee5d4028450ae7dda9
George Higbie <gehigbie@gmail.com>
2017-03-18 10:10:02 -0700

    add method to json.js

--- NotesNode/playground/json.js
+++ NotesNode/playground/json.js
@@ -5,3 +5,10 @@ var obj = {
 var stringObj = JSON.stringify(obj);
 console.log(typeof stringObj);
 console.log(stringObj);
+
+console.log("=========================");
+
+var personString = '{"name": "George", "age": 38}';
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
