4a0953e479cd9220cee7edb2d52677a49e0a06e1
George Higbie <gehigbie@gmail.com>
2017-03-18 08:18:10 -0700

    add command checking process

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -4,5 +4,16 @@ console.log("Starting app.js");
 const fs = require("fs");
 const _ = require("lodash");
 
-//developer created file
+//developer created files
 const notes = require("./notes.js");
+
+
+var command = process.argv[2];
+
+if(command  ==== "add"){
+	console.log("Adding new note");
+}else if(command === "list"){
+	console.log("Listing all notes");
+}else{
+	console.log("Command not recognized");
+}
