f880f427dfadc48ec1de4d74b7ff386f2b68235d
George Higbie <gehigbie@gmail.com>
2017-03-19 10:46:29 -0700

    add saveNotes() method

--- NotesNode/notes.js
+++ NotesNode/notes.js
@@ -5,30 +5,28 @@ const fs = require("fs");
 var fetchNotoes = () => {
 	try{
 		var notesString = fs.readFileSync("notes-data.json");
-		notes = JSON.parse(notesString);
+		return JSON.parse(notesString);
 	}catch (e){
-
+		return [];
 	}
-
 };
 
 var saveNotes = (notes) => {
-
+	fs.writeFileSync("notes-data.json", JSON.stringify(ndotes));
 };
 
 var addNote = (title, body) => {
-	var notes = [];
+	var notes = fetchNotes();
 	var note = {
 		title,
 		body
 	};
 
-
 	var duplicateNotes = notes.filter((note) => note.title === title);
 
 	if(duplicateNotes.length === 0){
 		notes.push(note);
-		fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+		saveNotes(notes);
 	}else{
 		console.log("===================================================");
 		console.log("A duplicate note was found. Message cannot be saved");
