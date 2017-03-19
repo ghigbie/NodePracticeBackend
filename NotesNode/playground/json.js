ce05e3070c8b755f826db6d9421baad66764a9fd
George Higbie <gehigbie@gmail.com>
2017-03-18 20:36:27 -0700

    add chain of methods to strigify and parse JSON

--- NotesNode/playground/json.js
+++ NotesNode/playground/json.js
@@ -1,14 +1,35 @@
-var obj = {
-	name: "George"
+// var obj = {
+// 	name: "George",
+// 	age: 34
+// };
+
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// console.log("=========================");
+
+// var personString = '{"name": "George", "age": 38}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require("fs");
+
+var originalNote = {
+	title: "Some title",
+	body: "Some body"
 };
 
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+var originalNoteString = JSON.stringify(originalNote);
+console.log(typeof originalNoteString);
+console.log(originalNoteString);
+fs.writeFileSync("notes.json", originalNoteString);
 
-console.log("=========================");
 
-var personString = '{"name": "George", "age": 38}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+console.log("===============================");
+
+var noteString = fs.readFileSync("notes.json");ote
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
